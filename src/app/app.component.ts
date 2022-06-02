e867690ccc694bb547ed2d6a9d48c6e6b5771edd
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-06-02 15:29:46 +0200

    functions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ interface bundesland {
   url: String;
 }
 
+let bundeslaender = [];
+let letters = [];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,5 +20,47 @@ export class AppComponent {
 
   constructor() {
     console.log(this.bundeslaender);
+  }
+
+  render(filter) {
+    let content = document.getElementById('content');
+    content.innerHTML = '';
+
+    for (let i = 0; i < bundeslaender.length; i++) {
+      const land = bundeslaender[i];
+      const population = (land['population'] + '').replace('.', ',');
+      const firstLetter = land['name'].charAt(0);
+
+      if (!filter || filter == firstLetter) {
+        content.innerHTML += generateLink(land, population);
+      }
+
+      if (!letters.includes(firstLetter)) {
+        letters.push(firstLetter);
+      }
+    }
+
+    renderLetters();
+  }
+
+  setFilter(letter) {
+    render(letter);
+  }
+
+  renderLetters() {
+    let letterbox = document.getElementById('letterbox');
+    letterbox.innerHTML = '';
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      letterbox.innerHTML += `<div onclick="setFilter('${letter}')" class="letter">${letter}</div>`;
+    }
+  }
+
+  generateLink(land, population) {
+    return `<a class="bbox" href="${land['url']}" target="_blank">
+    <div>${land['name']}</div>
+    <div class="text-gray">${population} Millionen</div>
+</a>`;
   }
 }
